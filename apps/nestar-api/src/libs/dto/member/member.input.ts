e159a990c1201c282aaacc1d917fbc69244c7b10
mike030188 <halol.imnida2023@gmail.com>
2024-05-11 19:42:34 +0900

    feat: integrate validation pipes logic

---
+++ apps/nestar-api/src/libs/dto/member/member.input.ts
@@ -0,0 +1,43 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { IsNotEmpty, IsOptional, Length } from 'class-validator';
+import { MemberAuthType, MemberType } from '../../enums/member.enum';
+
+/**SIGNUP **/
+@InputType()
+export class MemberInput {
+	@IsNotEmpty()
+	@Length(3, 12)
+	@Field(() => String)
+	memberNick: string;
+
+	@IsNotEmpty()
+	@Length(5, 12)
+	@Field(() => String)
+	memberPassword: string;
+
+	@IsNotEmpty()
+	@Field(() => String)
+	memberPhone: string;
+
+	@IsOptional() // bo`iliwi ham mumkin bo`lmasligi ham mumkin
+	@Field(() => MemberType, { nullable: true }) // bo`w boliwi mumkin
+	memberType?: MemberType;
+
+	@IsOptional()
+	@Field(() => MemberAuthType, { nullable: true })
+	memberAuthType?: MemberAuthType;
+}
+
+/**LOGIN **/
+@InputType()
+export class LoginInput {
+	@IsNotEmpty()
+	@Length(3, 12)
+	@Field(() => String)
+	memberNick: string;
+
+	@IsNotEmpty()
+	@Length(5, 12)
+	@Field(() => String)
+	memberPassword: string;
+}
