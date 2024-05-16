e338b8e833920dac4f36d9eca93a2030aedd0bcf
mike030188 <halol.imnida2023@gmail.com>
2024-05-17 05:20:00 +0900

    feat: create checkAuthRoles for testing

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -6,6 +6,9 @@ import { Member } from '../../libs/dto/member/member';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { MemberType } from '../../libs/enums/member.enum';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
 @Resolver()
 export class MemberResolver {
@@ -26,6 +29,7 @@ export class MemberResolver {
 		// }
 	}
 
+	// @UseGuards(RolesGuard) // test un wartli ravishda
 	@Mutation(() => Member) // GraphQL ham 'Member'ni qaytariwi kk
 	// @UsePipes(ValidationPipe) // method type pipe-validation
 	public async login(@Args('input') input: LoginInput): Promise<Member> {
@@ -57,6 +61,19 @@ export class MemberResolver {
 		return `Hi ${memberNick}`;
 	}
 
+	@Roles(MemberType.USER, MemberType.AGENT)
+	@UseGuards(RolesGuard)
+	@Query(() => String)
+	public async checkAuthRoles(@AuthMember() authMember: Member): Promise<string> {
+		console.log('Query: checkAuthRoles');
+		return `Hi ${authMember.memberNick}, you are ${authMember.memberType} (memberId: ${authMember._id})`;
+	}
+	// public async checkAuthRoles(@AuthMember('memberNick') memberNick: string): Promise<string> {
+	// 	console.log('Query: checkAuth');
+	// 	console.log('memberNick:', memberNick);
+	// 	return `Hi ${memberNick}`;
+	// }
+
 	@Query(() => String)
 	public async getMember(): Promise<string> {
 		console.log('Query: getMember');
@@ -66,8 +83,11 @@ export class MemberResolver {
 	/** ADMIN **/
 
 	// Authorization: ADMIN
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
 	@Mutation(() => String)
 	public async getAllMembersByAdmin(): Promise<string> {
+		// console.log('authMember.memberType:', authMember.memberType);
 		return this.memberService.getAllMembersByAdmin();
 	}
 
