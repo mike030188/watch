3b844b86a647d8dce0e8c63eae76a2c4626c2e3e
mike030188 <halol.imnida2023@gmail.com>
2024-05-29 18:58:06 +0900

    fix: modify getProperty & getBoardArticle business logic

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -102,6 +102,8 @@ export class MemberService {
 			}
 
 			// meLiked
+			const likeInput = { memberId: memberId, likeRefId: targetId, likeGroup: LikeGroup.MEMBER };
+			targetMember.meLiked = await this.likeService.checkLikeExistence(likeInput);
 			// meFollowed
 		}
 
--- apps/nestar-api/src/libs/dto/property/property.ts
+++ apps/nestar-api/src/libs/dto/property/property.ts
@@ -2,6 +2,7 @@ import { Field, Int, ObjectType } from '@nestjs/graphql';
 import { ObjectId } from 'mongoose';
 import { PropertyLocation, PropertyStatus, PropertyType } from '../../enums/property.enum';
 import { Member, TotalCounter } from '../member/member';
+import { MeLiked } from '../like/like';
 // import { MeLiked } from '../like/like';
 
 @ObjectType()
@@ -78,10 +79,10 @@ export class Property {
 	@Field(() => Date)
 	updatedAt: Date;
 
-	// 	/** from aggregation **/
+	/** from aggregation **/
 
-	// 	@Field(() => [MeLiked], { nullable: true })
-	// 	meLiked?: MeLiked[];
+	@Field(() => [MeLiked], { nullable: true })
+	meLiked?: MeLiked[];
 
 	@Field(() => Member, { nullable: true })
 	memberData?: Member;
