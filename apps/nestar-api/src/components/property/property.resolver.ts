995c1bfe0c941f793a0d723cda7eac8a9b5e0ea4
mike030188 <halol.imnida2023@gmail.com>
2024-06-02 05:33:31 +0900

    feat: develop getVisited GraphQL Api

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -82,6 +82,16 @@ export class PropertyResolver {
 		return await this.propertyService.getFavorites(memberId, input);
 	}
 
+	@UseGuards(AuthGuard)
+	@Query((returns) => Properties)
+	public async getVisited(
+		@Args('input') input: OrdinaryInquiry,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Properties> {
+		console.log('Query: getVisited');
+		return await this.propertyService.getVisited(memberId, input);
+	}
+
 	/** TARGET PLAY **/
 	@Roles(MemberType.AGENT)
 	@UseGuards(RolesGuard)
