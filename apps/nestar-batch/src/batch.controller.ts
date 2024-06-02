2f66d724b959c3f8e16184caa85bf034371c49f7
mike030188 <halol.imnida2023@gmail.com>
2024-06-02 23:08:47 +0900

    feat: create batchRollback, batchTopProperties, batchTopAgents jobSchedule logic

--- apps/nestar-batch/src/batch.controller.ts
+++ apps/nestar-batch/src/batch.controller.ts
@@ -1,12 +1,83 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { BatchService } from './batch.service';
+import { Cron, Interval, Timeout } from '@nestjs/schedule';
+import { BATCH_ROLLBACK, BATCH_TOP_AGENTS, BATCH_TOP_PROPERTIES } from './lib/config';
 
 @Controller()
 export class BatchController {
+	private logger: Logger = new Logger('BatchController');
+
 	constructor(private readonly batchService: BatchService) {}
+
+	@Timeout(1000)
+	handleTimeout() {
+		this.logger.debug('BATCH SERVER READY!');
+	}
+
+	@Cron('00 * * * * *', { name: BATCH_ROLLBACK })
+	public async batchRollback() {
+		try {
+			this.logger['context'] = BATCH_ROLLBACK;
+			this.logger.debug('EXECUTED!');
+			await this.batchService.batchRollback();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+
+	@Cron('20 * * * * *', { name: BATCH_TOP_PROPERTIES })
+	public async batchTopProperties() {
+		try {
+			this.logger['context'] = BATCH_TOP_PROPERTIES;
+			this.logger.debug('EXECUTED!');
+			await this.batchService.batchTopProperties();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+
+	@Cron('40 * * * * *', { name: BATCH_TOP_AGENTS })
+	public async batchTopAgents() {
+		try {
+			this.logger['context'] = BATCH_TOP_AGENTS;
+			this.logger.debug('EXECUTED!');
+			await this.batchService.batchTopAgents();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+
+	// @Cron('*/20 * * * * *', { name: 'CRON TEST' }) // 00 | */20 (* = 1min)
+	// public cronTest() {
+	// 	this.logger['context'] = 'CRON TEST';
+	// 	this.logger.debug('EXECUTED!');
+	// }
+
+	/*@Interval(1000)
+	handleInterval() {
+		this.logger.debug('INTERVAL TEST');
+	}
+	*/
 
 	@Get()
 	getHello(): string {
 		return this.batchService.getHello();
 	}
 }
+
+/*
+cron expressions:
+
+*    *    *    *    *    *
+|    |    |    |    |    |
+|    |    |    |    |    +----- day of the week (0 - 7) (Sunday=0 or 7)
+|    |    |    |    +---------- month (1 - 12)
+|    |    |    +--------------- day of the month (1 - 31)
+|    |    +-------------------- hour (0 - 23)
+|    +------------------------- minute (0 - 59)
++------------------------------ second (0 - 59)
+
+
+ task to run at the 0th second of every minute, every hour, every day, and so on. 
+ In other words, the task will be executed once every minute.
+*/
--- apps/nestar-batch/src/batch.service.ts
+++ apps/nestar-batch/src/batch.service.ts
@@ -2,7 +2,19 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class BatchService {
-	getHello(): string {
+	public async batchRollback(): Promise<void> {
+		console.log('batchRollback');
+	}
+
+	public async batchTopProperties(): Promise<void> {
+		console.log('batchTopProperties');
+	}
+
+	public async batchTopAgents(): Promise<void> {
+		console.log('batchTopAgents');
+	}
+
+	public getHello(): string {
 		return 'Welcome to Nestar BATCH server!';
 	}
 }
