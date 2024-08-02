d6dc146e4c505a0ca6dcaef310b34a54adb713e3
mike030188 <halol.imnida2023@gmail.com>
2024-08-03 06:32:40 +0900

    feat: develop FAQ related GraphQL APIs

--- apps/watches-api/src/components/faq/faq.resolver.ts
+++ apps/watches-api/src/components/faq/faq.resolver.ts
@@ -1,4 +1,71 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { FaqService } from './faq.service';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { MemberType } from '../../libs/enums/member.enum';
+import { UseGuards } from '@nestjs/common';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Faq, Faqs } from '../../libs/dto/faq/faq';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { FaqInput, FaqInquiry } from '../../libs/dto/faq/faq.input';
+import { ObjectId } from 'mongoose';
+import { FaqUpdate } from '../../libs/dto/faq/faq.update';
+import { shapeIntoMongoObjectId } from '../../libs/config';
+import { WithoutGuard } from '../auth/guards/without.guard';
 
 @Resolver()
-export class FaqResolver {}
+export class FaqResolver {
+	constructor(private readonly faqService: FaqService) {}
+
+	/** ADMIN **/
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Mutation((returns) => Faq)
+	public async createFaq(@Args('input') input: FaqInput, @AuthMember('_id') memberId: ObjectId): Promise<Faq> {
+		console.log('Mutation: createFaq');
+		console.log(input, '***************');
+
+		const data = await this.faqService.createFaq(memberId, input);
+		return data;
+	}
+
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Mutation((returns) => Faq)
+	public async updateFaq(@Args('input') input: FaqUpdate, @AuthMember('_id') memberId: ObjectId): Promise<Faq> {
+		console.log('Mutation: updateFaq');
+		const data = await this.faqService.updateFaq(memberId, input);
+		return data;
+	}
+
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Mutation((returns) => Faq)
+	public async deleteFaq(@Args('input') input: string, @AuthMember('_id') memberId: ObjectId): Promise<Faq> {
+		console.log('Mutation: deleteFaq');
+		const faqId = shapeIntoMongoObjectId(input);
+		const data = await this.faqService.deleteFaq(faqId);
+		return data;
+	}
+
+	/** CLIENT **/
+
+	@UseGuards(WithoutGuard)
+	@Query((returns) => Faq)
+	public async getFaq(@Args('input') input: string, @AuthMember('_id') memberId: ObjectId): Promise<Faq> {
+		console.log('Query: getFaq');
+		const faqId = shapeIntoMongoObjectId(input);
+		const data = this.faqService.getFaq(faqId);
+
+		return data;
+	}
+
+	@UseGuards(WithoutGuard)
+	@Query((returns) => Faqs)
+	public async getFaqs(@Args('input') input: FaqInquiry, @AuthMember('_id') memberId: ObjectId): Promise<Faqs> {
+		console.log('Query: getFaqs');
+
+		const data = this.faqService.getFaqs(memberId, input);
+
+		return data;
+	}
+}
--- apps/watches-api/src/components/faq/faq.service.ts
+++ apps/watches-api/src/components/faq/faq.service.ts
@@ -1,4 +1,105 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Faq, Faqs } from '../../libs/dto/faq/faq';
+import { Model, ObjectId } from 'mongoose';
+import { FaqInput, FaqInquiry } from '../../libs/dto/faq/faq.input';
+import { Message } from '../../libs/enums/common.enum';
+import { FaqUpdate } from '../../libs/dto/faq/faq.update';
+import { T } from '../../libs/types/common';
 
 @Injectable()
-export class FaqService {}
+export class FaqService {
+	constructor(@InjectModel('Faq') private readonly faqModel: Model<Faq>) {}
+
+	/** ADMIN**/
+	public async createFaq(memberId: ObjectId, input: FaqInput): Promise<Faq> {
+		input.memberId = memberId;
+
+		const result: Faq = await this.faqModel.create(input);
+
+		if (!result) throw new InternalServerErrorException(Message.CREATE_FAILED);
+
+		return result;
+	}
+
+	public async updateFaq(memberId: ObjectId, input: FaqUpdate): Promise<Faq> {
+		console.log(input, 'FAQ INPUT');
+
+		const result: Faq = await this.faqModel
+			.findOneAndUpdate({ _id: input._id, memberId: memberId }, input, {
+				new: true,
+			})
+			.exec();
+
+		if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+
+		return result;
+	}
+
+	public async deleteFaq(faqId: ObjectId): Promise<Faq> {
+		const result: Faq = await this.faqModel.findOneAndDelete(faqId).exec();
+
+		if (!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
+
+		return result;
+	}
+
+	/** CLIENT **/
+
+	public async getFaq(faqId: ObjectId): Promise<Faq> {
+		const result: Faq = await this.faqModel.findOne(faqId).exec();
+
+		if (!result) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+		return result;
+	}
+
+	public async getFaqs(memberId: ObjectId, input: FaqInquiry): Promise<Faqs> {
+		const { faqType, faqStatus, text } = input;
+		const match: T = {};
+		if (faqType) {
+			match.faqType = faqType;
+		}
+		if (faqStatus) {
+			match.faqStatus = faqStatus;
+		}
+		if (text) {
+			// match.faqAnswer = { $regex: new RegExp(text, 'i') };
+			match.faqQuestion = { $regex: new RegExp(text, 'i') };
+		}
+
+		const sort: T = { ['createdAt']: -1 };
+		const result = await this.faqModel
+			.aggregate([
+				{ $match: match },
+				{ $sort: sort },
+				{
+					$facet: {
+						list: [{ $skip: (input.page - 1) * input.limit }, { $limit: input.limit }],
+						metaCounter: [{ $count: 'total' }],
+					},
+				},
+			])
+			.exec();
+
+		if (!result || !result[0]) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+		const faqsResult = result[0];
+
+		const faqsDto: Faqs = {
+			list: faqsResult.list.map((item: Faq) => ({
+				_id: item._id,
+				faqQuestion: item.faqQuestion,
+				faqAnswer: item.faqAnswer,
+				faqType: item.faqType,
+				faqStatus: item.faqStatus,
+				memberData: item.memberData,
+				createdAt: item.createdAt,
+				updatedAt: item.updatedAt,
+			})),
+			metaCounter: faqsResult.metaCounter,
+		};
+
+		return faqsDto;
+	}
+}
