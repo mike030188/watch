6aefde9ea9952b86dddb9ef542aeff1045aacbd2
mike030188 <halol.imnida2023@gmail.com>
2024-08-02 19:16:29 +0900

    fix: modify notice enum

--- apps/watches-api/src/libs/enums/notice.enum.ts
+++ apps/watches-api/src/libs/enums/notice.enum.ts
@@ -17,3 +17,16 @@ export enum NoticeStatus {
 registerEnumType(NoticeStatus, {
 	name: 'NoticeStatus',
 });
+
+export enum NoticeType {
+	PROMOTION = 'PROMOTION',
+	NEW_ARRIVAL = 'NEW_ARRIVAL',
+	MAINTENANCE = 'MAINTENANCE',
+	SAFETY = 'SAFETY',
+	WEBSITE_UPDATE = 'WEBSITE_UPDATE',
+	LEGAL_POLICY = 'LEGAL_POLICY',
+	GENERAL_ANNOUNCEMENT = 'GENERAL_ANNOUNCEMENT',
+}
+registerEnumType(NoticeType, {
+	name: 'NoticeType',
+});
