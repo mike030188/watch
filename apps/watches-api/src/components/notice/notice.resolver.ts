e68ba833ac4267c66b0fa7940970755ecb4259eb
mike030188 <halol.imnida2023@gmail.com>
2024-08-03 06:11:10 +0900

    feat: developed Notice related GraphQL APIs

--- apps/watches-api/src/components/notice/notice.resolver.ts
+++ apps/watches-api/src/components/notice/notice.resolver.ts
@@ -1,4 +1,74 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { NoticeService } from './notice.service';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { MemberType } from '../../libs/enums/member.enum';
+import { UseGuards } from '@nestjs/common';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Notice, Notices } from '../../libs/dto/notice/notice';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { NoticeInput, NoticeInquiry } from '../../libs/dto/notice/notice.input';
+import { ObjectId } from 'mongoose';
+import { NoticeUpdate } from '../../libs/dto/notice/notice.update';
+import { shapeIntoMongoObjectId } from '../../libs/config';
+import { WithoutGuard } from '../auth/guards/without.guard';
 
 @Resolver()
-export class NoticeResolver {}
+export class NoticeResolver {
+	constructor(private readonly noticeService: NoticeService) {}
+
+	/** ADMIN **/
+	@Roles(MemberType.ADMIN, MemberType.AGENT)
+	@UseGuards(RolesGuard)
+	@Mutation((returns) => Notice)
+	public async createNotice(@Args('input') input: NoticeInput, @AuthMember('_id') memberId: ObjectId): Promise<Notice> {
+		console.log('Mutation: createNotice');
+		input.memberId = memberId;
+		const data = await this.noticeService.createNotice(input);
+		return data;
+	}
+
+	@Roles(MemberType.ADMIN, MemberType.AGENT)
+	@UseGuards(RolesGuard)
+	@Mutation((returns) => Notice)
+	public async updateNotice(
+		@Args('input') input: NoticeUpdate,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Notice> {
+		console.log('Mutation: updateNotice');
+		const data = await this.noticeService.updateNotice(memberId, input);
+		return data;
+	}
+
+	@Roles(MemberType.ADMIN, MemberType.AGENT)
+	@UseGuards(RolesGuard)
+	@Mutation((returns) => Notice)
+	public async deleteNotice(@Args('input') input: string, @AuthMember('_id') memberId: ObjectId): Promise<Notice> {
+		console.log('Mutation: deleteNotice');
+		const noticeId = shapeIntoMongoObjectId(input);
+		const data = await this.noticeService.deleteNotice(noticeId);
+		return data;
+	}
+
+	/** CLIENT **/
+
+	@UseGuards(WithoutGuard)
+	@Query((returns) => Notice)
+	public async getNotice(@Args('input') input: string, @AuthMember('_id') memberId: ObjectId): Promise<Notice> {
+		console.log('Query: getNotice');
+		const noticeId = shapeIntoMongoObjectId(input);
+		const data = this.noticeService.getNotice(noticeId);
+
+		return data;
+	}
+
+	@UseGuards(WithoutGuard)
+	@Query((returns) => Notices)
+	public async getNotices(
+		@Args('input') input: NoticeInquiry,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Notices> {
+		console.log('Query: getNotices');
+		const data = this.noticeService.getNotices(memberId, input);
+		return data;
+	}
+}
--- apps/watches-api/src/components/notice/notice.service.ts
+++ apps/watches-api/src/components/notice/notice.service.ts
@@ -1,4 +1,97 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Notice, Notices } from '../../libs/dto/notice/notice';
+import { Model, ObjectId } from 'mongoose';
+import { MemberService } from '../member/member.service';
+import { NoticeInput, NoticeInquiry } from '../../libs/dto/notice/notice.input';
+import { Message } from '../../libs/enums/common.enum';
+import { NoticeUpdate } from '../../libs/dto/notice/notice.update';
+import { NoticeStatus } from '../../libs/enums/notice.enum';
+import { T } from '../../libs/types/common';
 
 @Injectable()
-export class NoticeService {}
+export class NoticeService {
+	constructor(
+		@InjectModel('Notice') private readonly noticeModel: Model<Notice>,
+		private memberService: MemberService,
+	) {}
+
+	/** ADMIN **/
+	public async createNotice(input: NoticeInput): Promise<Notice> {
+		try {
+			const result = await this.noticeModel.create(input);
+
+			return result;
+		} catch (err) {
+			console.log('Error, Service.model:', err.message);
+			throw new BadRequestException(Message.CREATE_FAILED);
+		}
+	}
+
+	public async updateNotice(memberId: ObjectId, input: NoticeUpdate): Promise<Notice> {
+		const result: Notice = await this.noticeModel
+			.findOneAndUpdate({ _id: input._id, memberId: memberId }, input, { new: true })
+			.exec();
+
+		if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+
+		return result;
+	}
+
+	public async deleteNotice(noticeId: ObjectId): Promise<Notice> {
+		const result: Notice = await this.noticeModel.findOneAndDelete(noticeId).exec();
+
+		if (!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
+
+		return result;
+	}
+
+	/** Client **/
+
+	public async getNotice(noticeId: ObjectId): Promise<Notice> {
+		const result: Notice = await this.noticeModel.findOne(noticeId).exec();
+
+		if (!result) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+		return result;
+	}
+
+	public async getNotices(memberId: ObjectId, input: NoticeInquiry): Promise<Notices> {
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		const { noticeType } = input;
+
+		const match: T = { noticeStatus: NoticeStatus.ACTIVE };
+		const sort: T = { ['createdAt']: -1 };
+		const result = await this.noticeModel
+			.aggregate([
+				{ $match: match },
+				{ $sort: sort },
+				{
+					$facet: {
+						list: [{ $skip: (input.page - 1) * input.limit }, { $limit: input.limit }],
+						metaCounter: [{ $count: 'total' }],
+					},
+				},
+			])
+			.exec();
+
+		if (!result || !result[0]) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+		const noticesResult = result[0];
+
+		const notices: Notices = {
+			list: noticesResult.list.map((item: Notice) => ({
+				_id: item._id,
+				noticeContent: item.noticeContent,
+				noticeType: item.noticeType,
+				memberData: item.memberData,
+				noticeStatus: item.noticeStatus,
+				createdAt: item.createdAt,
+				updatedAt: item.updatedAt,
+			})),
+			metaCounter: noticesResult.metaCounter,
+		};
+
+		return notices;
+	}
+}
