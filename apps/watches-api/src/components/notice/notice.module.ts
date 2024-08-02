0ab164e5b92a243e0db4a7124a970ea3be90e979
mike030188 <halol.imnida2023@gmail.com>
2024-08-02 19:01:00 +0900

    feat: create components related NoticeModule

---
+++ apps/watches-api/src/components/notice/notice.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { NoticeResolver } from './notice.resolver';
+import { NoticeService } from './notice.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MemberModule } from '../member/member.module';
+import { AuthModule } from '../auth/auth.module';
+
+@Module({
+	imports: [
+		MongooseModule.forFeature([
+			{
+				name: 'Notice',
+				schema: 'NoticeSchema',
+			},
+		]),
+		MemberModule,
+		AuthModule,
+	],
+	providers: [NoticeResolver, NoticeService],
+})
+export class NoticeModule {}
